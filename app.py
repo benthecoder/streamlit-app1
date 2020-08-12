34b6deca488298bce0a43a1ee9ec42009931b7fc
benthecoder <benthecoder07@gmail.com>
2020-08-12 13:55:23 +0800

    added imports

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+@st.cache
+def get_data():
+    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv"
+    return pd.read_csv(url)
+df = get_data()
 
 st.title("Hello streamlit")
 st.subheader("My first streamlit app")
 st.write("COVID-19 dashboard app coming soon")
+
+st.dataframe(df.head())
